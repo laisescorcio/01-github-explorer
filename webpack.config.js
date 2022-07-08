5064f11d9c64bbab9a7f70a78156140d5f148319
laisescorcio <lais.correia@outlook.com>
2022-07-07 21:02:17 -0300

    feat: render Hello World, import React automatically by babel and added development webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'development', // modo de desenvolvimento do webpack - evita de rodar otimizações que seriam feitas em produção
     entry: path.resolve(__dirname, 'src', 'index.jsx'), // insere a barra correta de acordo com o sistema operacional
     output: { // arquivo que será gerado do webpack
         path: path.resolve(__dirname, 'dist'), // será gerado na pasta dist
